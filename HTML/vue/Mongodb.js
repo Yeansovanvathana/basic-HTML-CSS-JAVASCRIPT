1f628364183aada6a99c6c7aaf576d45d68f8af2
Yeansovanvathana <yean.sovanvathana19@kit.edu.kh>
2020-11-25 17:24:55 +0700

    HTML<CSS<JAVASCRIPT

---
+++ HTML/vue/Mongodb.js
@@ -0,0 +1,13 @@
+db.records.insertMany([ 
+  {  id: 134,
+     name: "Vichea",
+     phone: 12345, 
+     current_date: “23 sep 2020” 
+  }, 
+  {  id: 1250, 
+     name: "Phalin", 
+     phone: 132345, 
+     current_date: "23 sep 2020" },
+  {id: 126, name: "Vichea", current_date: "23 sep 2020", address: { street: "West street", city : "Phnom Penh" }, favourites: ["Music", "Reading books", "Singing"] 
+  } 
+])
